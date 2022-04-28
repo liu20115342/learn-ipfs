82cf600140958e11c0f65bb94d31f358e6f970ed
liujian <jian.liu@okg.com>
2022-04-28 12:48:07 +0800

    build: 区分环境打包

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const isDev = process.env.NODE_ENV === 'development';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: isDev ? '/' : './',
+  plugins: [react()],
 })
